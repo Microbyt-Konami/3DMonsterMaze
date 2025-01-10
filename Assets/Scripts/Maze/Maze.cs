e3ae275ac0a98e6fd2b5aef6bc382085717234ef
Oscar Devs <ohernandezba@gmail.com>
2025-01-10 16:45:14 +0100

    test maze

--- Assets/Scripts/Maze/Maze.cs
+++ Assets/Scripts/Maze/Maze.cs
@@ -1,12 +1,38 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class Maze : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer floorRenderer;
+    //[SerializeField] private MeshRenderer floorRenderer;
+    [SerializeField] private Vector3 cellSize = new Vector3(4, 4, 4);
+    [SerializeField] private float thickness = 0.1f;
+    [SerializeField] private GameObject floorPrefab;
     [SerializeField] private GameObject wallPrefab;
 
     public int rows, columns;
     public Cell[,] cells;
+
+    private GameObject _floorGO;
+
+    public void AddFloor()
+    {
+        _floorGO = Instantiate(floorPrefab, Vector3.zero, Quaternion.identity, transform);
+        _floorGO.transform.localScale = new Vector3(cellSize.x * columns, thickness, cellSize.z * rows);
+
+        var floorRenderer = _floorGO.GetComponent<MeshRenderer>();
+        var material = new Material(floorRenderer.material);
+
+        material.mainTextureScale = new Vector2(columns / 4.0f, rows / 4.0f);
+        floorRenderer.material = material;
+    }
+
+    public void AddWallV(int row, int column, int ncell = 1, Material material = null)
+    {
+        var position = new Vector3((row / rows - (rows / 2)) * cellSize.x, cellSize.x / 2, (column / columns - (columns / 2)) * cellSize.y);
+        var wall = Instantiate(wallPrefab, position, Quaternion.identity, transform);
+
+        wall.transform.localScale = new Vector3(1, 1, cellSize.x * ncell);
+    }
 
     public void UpdateFloor()
     {
@@ -14,7 +40,7 @@ public class Maze : MonoBehaviour
 
         //material.mainTextureScale = new Vector2(columns, rows);
         //floorRenderer.material = material;
-        floorRenderer.transform.localScale = new Vector3(4 * columns, 0.1f, 4 * rows);
+        //floorRenderer.transform.localScale = new Vector3(4 * columns, 0.1f, 4 * rows);
     }
 
     public Cell GetCell(int row, int column)
--- Assets/Tests/TestMaze/TestWall1.cs
+++ Assets/Tests/TestMaze/TestWall1.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class TestWall1 : MonoBehaviour
 {
     [SerializeField] private Maze maze;
+    [SerializeField] private Material[] wallMaterials;
 
     private void Start()
     {
         maze.rows = maze.columns = 16;
-        maze.UpdateFloor();
+        maze.AddFloor();
+        maze.AddWallV(0, 0, 3);
     }
 }
